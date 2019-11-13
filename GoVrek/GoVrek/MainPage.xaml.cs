ad3a90e70706726b2ea4e9a2ba593a95122eb470
Jermaine217 <55493849+Jermaine217@users.noreply.github.com>
2019-11-13 10:09:06 +0200

    UPDATE

---
+++ GoVrek/GoVrek/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GoVrek
+{
+
+    public partial class MainPage : ContentPage
+    {
+
+        static string bomb = new Random().Next(1, 11).ToString();
+
+        static int scores = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+
+            Button button = sender as Button;
+            if (button.Text == bomb)
+            {
+                scores = 0;
+
+                await DisplayAlert("Bomb Exploded", "Go Home Noob", "Or Try Again");
+                bomb = new Random().Next(1, 12) .ToString();
+            }
+            else
+            {
+                scores += 1;
+
+                await DisplayAlert("Bomb Defused!", "Score " + scores, "Continue");
+                bomb = new Random().Next(1, 12).ToString();
+
+
+
+                /// i add this in my self hoping it will work ////
+
+
+                Button buttonz = sender as Button;
+                if (button.Text == bomb)
+                {
+                    scores = 0;
+
+                    await DisplayAlert("Bomb Exploded", "Go Home Noob", "Or Try Again");
+                    bomb = new Random().Next(11, 22).ToString();
+                }
+                else
+                {
+                    scores += 1;
+
+                    await DisplayAlert("Bomb Defused!", "Score " + scores, "Continue");
+                    bomb = new Random().Next(11, 22).ToString();
+                 
+
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
