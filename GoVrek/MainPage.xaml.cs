3de13d4f6ca9df627b028b1f405ac2a4c3f5e6d0
Jermaine217 <55493849+Jermaine217@users.noreply.github.com>
2019-11-20 15:16:55 +0200

    update

---
+++ GoVrek/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GoVrek
+{
+
+    public partial class MainPage : ContentPage
+    {
+
+        static string bomb = new Random().Next(1, 7).ToString();
+
+        static int scores = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+
+            Button button = sender as Button;
+            if (button.Text == bomb)
+            {
+                scores = 0;
+
+                await DisplayAlert("Bomb Exploded", "Go Home Noob", "Or Try Again");
+                bomb = new Random().Next(1, 7) .ToString();
+            }
+            else
+            {
+                scores += 1;
+
+                await DisplayAlert("Bomb Defused!", "Score " + scores, "Continue");
+                bomb = new Random().Next(1, 7).ToString();
+
+
+
+                
+
+
+                
+            }
+
+        }
+    }
+}
